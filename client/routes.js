0d627ae3685e236edcced6521e44df2c48c3456c
ricky <rickyrojas@me.com>
2018-03-17 18:59:20 -0400

    first commit

---
+++ client/routes.js
@@ -0,0 +1,8 @@
+//client/routes.js
+import React from 'react';
+import {Route, Switch} from 'react-router-dom';
+import App from './components/app';
+export const Routes = () => (<Switch>
+  <Route path='/' component={App}/>
+</Switch>);
+export default Routes;
